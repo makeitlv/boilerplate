e595177f2f7da1ffb39c162f82e67483cbe52718
Viktor Vipolzov <viktor.vipolzov@gmail.com>
2024-08-14 20:45:42 +0300

    Fixes

--- migrations/Version20240814174400.php
+++ migrations/Version20240814174400.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240814164200 extends AbstractMigration
+final class Version20240814174400 extends AbstractMigration
 {
     #[\Override]
     public function getDescription(): string
@@ -22,8 +22,8 @@ final class Version20240814164200 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE book (uuid UUID NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, firstname VARCHAR(64) NOT NULL, lastname VARCHAR(64) NOT NULL, PRIMARY KEY(uuid))');
-        $this->addSql("COMMENT ON COLUMN book.uuid IS '(DC2Type:uuid)'");
+        $this->addSql('CREATE TABLE books (uuid UUID NOT NULL, title VARCHAR(255) NOT NULL, description TEXT NOT NULL, firstname VARCHAR(64) NOT NULL, lastname VARCHAR(64) NOT NULL, PRIMARY KEY(uuid))');
+        $this->addSql("COMMENT ON COLUMN books.uuid IS '(DC2Type:uuid)'");
         $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
         $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
@@ -46,7 +46,7 @@ final class Version20240814164200 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE books');
         $this->addSql('DROP TABLE messenger_messages');
     }
 }
